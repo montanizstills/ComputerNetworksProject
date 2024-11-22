c5c3d8285a9ec179bccb5b4098ce4ce210a5f9bc
montanizstills <montanizstills@gmail.com>
2024-11-22 11:46:52 -0500

    stub POST method implementation;

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from crypt import methods
+
 from flask import Flask
 
 from tcp_helper import run_server
@@ -5,9 +7,16 @@ from tcp_helper import run_server
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def hello_world():
     return """Hello, World!"""
+
+
+@app.route('/', methods=['POST'])
+def user_clicks_send_button():
+    # get data from HTML input field
+    # take data and send to server
+    pass
 
 
 if __name__ == '__main__':
