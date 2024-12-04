02aeff26cacdba2c882c520991506b5882aa269f
montanizstills <montanizstills@gmail.com>
2024-12-03 19:39:41 -0500

    show connection metrics;

--- tcp_helper.py
+++ tcp_helper.py
@@ -24,12 +24,18 @@ def udp_server(server_name, server_port):
     server_socket = create_udp_socket()
     server_socket.bind((server_name, server_port))
     print("The server is ready to receive!")
+
+    CONNECTED_CLIENTS = {}
     while True:
         message, client_address = server_socket.recvfrom(2048)
+        if client_address not in CONNECTED_CLIENTS:
+            print(f'Connection from {client_address}')
+        CONNECTED_CLIENTS[client_address] = server_socket
+
+        # for client_address, server_socket in CONNECTED_CLIENTS.items():
+        #     server_socket.sendto(message, client_address)
+
         message = message.decode('utf-8')
-        # print(f'{client_address}: {message}')
-        # Here we need to send to EVERY client ##############################
-        message = f"{client_address}: {message}"
-        message = message.encode('utf-8')
-        server_socket.sendto(message, client_address)
+        print(f'{client_address}: {message}')
+
     server_socket.close()
